5d74d8b8da519a31b42001dad0ffc7bad89c5c5a
Vukasin211 <vukasinp@elfak.rs>
2022-11-06 16:53:37 +0100

    Projekat 1 skoro gotv

---
+++ publicApi .NET/publicApi/publicApi/Models/Currency .cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace publicApi.Models
+{
+    public class Currency
+    {
+        public int currencyId { get; set; }
+        public string title { get; set; }
+        public string currency { get; set; }
+        public float value { get; set; }
+        public string data { get; set; }
+    }
+}
+
+/*
+  const CurrencySchema = mongoose.Schema({
+  currencyId: {
+    type: Number,
+    required: true,
+    index: { unique: true }
+  },  
+  title: String,
+  currency: String,
+  value: Number,
+  date: String
+});
+*/
